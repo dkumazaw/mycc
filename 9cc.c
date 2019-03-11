371e16887bc2f702c31fdee801aaaf99d9edafc4
dkumazaw <dkumazaw@stanford.edu>
2019-03-11 18:28:02 +0900

    Add test for vector

--- 9cc.c
+++ 9cc.c
@@ -165,6 +165,28 @@ void tokenize(char *p) {
     tokens[i].input = p;
 }
 
+int expect(int line, int expected, int actual) {
+    if (expected == actual)
+        return;
+    fprintf(stderr, "%d: %d expected, but got %d\n", line, expected, actual);
+    exit(1);
+}
+
+void runtest() {
+    Vector *vec = new_vector();
+    expect(__LINE__, 0, vec->len);
+
+    for (int i = 0; i < 100; i++)
+        vec_push(vec, (void *)i);
+    
+    expect(__LINE__, 100, vec->len);
+    expect(__LINE__, 0, (int)vec->data[0]);
+    expect(__LINE__, 50, (int)vec->data[50]);
+    expect(__LINE__, 99, (int)vec->data[99]);
+
+    printf("OK\n")
+}
+
 // Reports error
 void error(int i) {
     fprintf(stderr, "Unexpected token: %d\n",
