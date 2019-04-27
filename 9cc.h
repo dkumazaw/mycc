3175aaa3da98f25cfe1500cd972377c876aa30a5
dkumazaw <dkumazaw@stanford.edu>
2019-04-27 15:22:48 +0900

    Add some comments for readability

--- 9cc.h
+++ 9cc.h
@@ -3,29 +3,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum {
+enum
+{
     TK_NUM = 256, // integer token
-    TK_EOF, // token representing end of input
+    TK_EOF,       // token representing end of input
 };
 
-enum {
+enum
+{
     ND_NUM = 256, // integer node type
 };
 
-typedef struct {
-    int ty; // type of token
-    int val; // if ty is TK_NUM, store its value
+/*
+struct Token
+
+int ty: TK_NUM if the content is an integer,
+        ASCII expression if the content is an operator like '+',
+        TK_EOF if the content is the end of input
+int val: set to the integer value if ty is TK_NUM
+char *input: pointer to the start of the token
+*/
+typedef struct
+{
+    int ty;      // type of token
+    int val;     // if ty is TK_NUM, store its value
     char *input; // token str (for error message)
 } Token;
 
-typedef struct Node {
+typedef struct Node
+{
     int ty; // operator or ND_NUM
     struct Node *lhs;
     struct Node *rhs;
     int val;
 } Node;
 
-typedef struct {
+typedef struct
+{
     void **data;
     int capacity;
     int len;
